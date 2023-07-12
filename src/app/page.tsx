cbaed1fd3184841ff0dc854834cfbf68e5e5a2da
Renato Wéssmėr <rewesa@outlook.com.br>
2023-07-12 05:40:41 -0300

    build: elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,62 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={100}
-        height={24}
-        priority
-      />
+      <div className="header">
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          width={100}
+          height={24}
+          priority
+        />
+      </div>
+      <div>
+        <h1>
+          Hi, i am Renato!
+        </h1>
+        <h2>
+          Estudante de desenvolvimento de software e cloud
+        </h2>
+      </div>
+      <div className="experience">
+        <h3>
+          Expirience
+        </h3>
+        <p>
+          Texto
+        </p>
+        <div className="experience-time">
+
+        </div>
+        <div className="infos">
+          <h3>Languages</h3>
+          <div className="languages-info">
+            <span>🇺🇸 EN - Avançado</span>
+            <span>🇧🇷 PT-BR - Native Speaker</span>
+          </div>
+          <h3>Education</h3>
+          <div className="educational-info">
+            <span>
+              🎓
+            </span>
+            <span>
+              Universidade
+            </span>
+          </div>
+          
+        </div>
+        <div className="buttons">
+          <div className="social">
+
+          </div>
+          <button>
+            contact me
+          </button>
+
+        </div>
+
+
+      </div>
     </main>
   )
 }
